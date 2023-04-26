08ea4f8d209cd42da281a792b150ca743ed8365d
Splendidempire6307 <timli6307@gmail.com>
2023-04-26 20:30:36 +0800

    V9

--- app.py
+++ app.py
@@ -20,13 +20,9 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# 填入你的 LINE 機器人 Channel Access Token
 line_bot_api = LineBotApi(t.Token)
-
-# 填入你的 LINE 機器人 Channel Secret
 handler = WebhookHandler(t.Key)
 
-# 設定 Webhook URL
 @app.route("/callback", methods=['POST'])
 def callback():
     # 取得 Line Server 回傳的 Header 資料
@@ -43,7 +39,6 @@ def callback():
 
     return 'OK'
 
-# 讀取 Excel 檔案中的資料
 df = pd.read_excel("test.xlsx", sheet_name= '工作表1')
 
 #使用者加入Bot後 傳送歡迎訊息 訊息要改成歡迎+自我介紹+功能說明
@@ -67,19 +62,14 @@ def handle_message(event):
         line_bot_api.push_message(event.source.user_id, TextSendMessage(text=message))
 
     if '超標工廠' in event.message.text:
-        # 過濾 DataFrame 中排放量大於等於 30 的工廠數據
         df_over_limit = df.query('碳排放 >= 30')
-        # 設置 message 變數為空字符串
         message = ""
-        # 遍歷 df_over_limit 中的每行數據
         for i, row_data in df_over_limit.iterrows():
             message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
-        # 如果 message 不為空，則將其推送到使用者的 LINE 帳號中
         if message:
             line_bot_api.push_message(event.source.user_id, TextSendMessage(text=message))
     buttons_template = None
     if event.message.text == "menu":
-        # create a button template message
         buttons_template = ButtonsTemplate(
             title="請選擇",
             text="您要選擇全部工廠還是選擇縣市？",
@@ -126,5 +116,4 @@ def handle_postback(event):
                 message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
         line_bot_api.push_message(event.source.user_id, TextSendMessage(text=message))
 if __name__ == "__main__":
-    # 執行 Flask Server
     app.run()
