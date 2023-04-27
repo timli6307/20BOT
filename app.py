3104513f4a9d55d0cbb3891c7e2c96fa06f2eb5c
XD <wesleylin30@gmail.com>
2023-04-27 17:22:48 +0800

    V10

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from linebot.exceptions import (
     LineBotApiError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,FollowEvent,
+    MessageEvent, TextMessage, TextSendMessage,FollowEvent,UnfollowEvent,
     TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction,PostbackEvent,MessageTemplateAction,
     PostbackAction,
     QuickReply, QuickReplyButton, MessageAction
@@ -42,19 +42,6 @@ def callback():
 
 df1 = pd.read_excel("test.xlsx", sheet_name= '工作表1')
 df2 = pd.read_excel('compare.xlsx', sheet_name='工作表1')
-diff = df1.compare(df2)#照寫
-if not diff.empty:
-    df1_over_limit = df1.query('碳排放 >= 30')
-    message = ""
-    for i, row_data in df1_over_limit.iterrows():
-        message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
-        if message:
-            line_bot_api.push_message(event.source.user_id, TextSendMessage(text=message))#XXXXXXXXX
-    df2 = pd.DataFrame(df1)
-df2.to_excel('compare.xlsx', sheet_name = '工作表1',index=False)
-
-#加入好友時 把user_id add到excel
-
 
 #使用者加入Bot後 傳送歡迎訊息 訊息要改成歡迎+自我介紹+功能說明
 @handler.add(FollowEvent)
@@ -62,7 +49,62 @@ def handle_follow(event):
     user_id = event.source.user_id
     message = TextSendMessage(text="歡迎加入Line 廢水監控機器人！\n輸入「所有」查看所有工廠數據\n輸入「超標工廠」查看所有超標工廠數據\n輸入「menu」查看各縣市工廠數據")
     line_bot_api.push_message(user_id, message)
+    df = pd.read_excel('friendList.xlsx',sheet_name= 'Sheet1')
+    if user_id not in df['user_id'].tolist():
+        # 加入新的好友# 取得新加入好友的user_id
 
+        # 檢查是否已經加入過好友
+        if user_id in df['user_id'].values:
+            print('user_id已存在')
+        else:
+            # 將user_id新增至Excel
+            new_user = {'user_id': user_id}
+            df = df.append(new_user, ignore_index=True)
+
+            # 儲存更新後的Excel檔案
+            df.to_excel('friendList.xlsx', index=False)
+            print('已新增user_id:', user_id)
+
+#刪除好友後將user_id從excel中移除
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    user_id = event.source.user_id
+    # 讀取現有的Excel檔案
+    df = pd.read_excel('friendList.xlsx')
+
+    # 檢查user_id是否存在
+    if user_id not in df['user_id'].values:
+        print('user_id不存在')
+    else:
+        # 找出要刪除的user_id所在的索引位置
+        index = df[df['user_id'] == user_id].index
+
+        # 刪除該索引位置的資料
+        df.drop(index, inplace=True)
+
+        # 重新編號索引
+        df.reset_index(drop=True, inplace=True)
+
+        # 儲存更新後的Excel檔案
+        df.to_excel('friendList.xlsx', index=False)
+        print('已刪除user_id:', user_id)
+
+
+
+diff = df1.compare(df2)#照寫
+if not diff.empty:
+    df1_over_limit = df1.query('碳排放 >= 30')
+    message = ""
+    for i, row_data in df1_over_limit.iterrows():
+        message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
+    df = pd.read_excel('friendList.xlsx',sheet_name= 'Sheet1')
+    for i ,row_data in df.iterrows():
+        line_bot_api.push_message(row_data['user_id'], TextSendMessage(text=message))
+    
+    # 將 DataFrame 儲存到 Excel 文件中
+    with pd.ExcelWriter('compare.xlsx') as writer:
+        df1.to_excel(writer, sheet_name='工作表1', index=False)
+    
 # 當接收到 MessageEvent (文字訊息) 時的處理函式
 @handler.add(MessageEvent, message=TextMessage)#根據取得數據需要去做修改
 def handle_message(event):
--- test.py
+++ test.py
@@ -1,5 +1,21 @@
+"""import pandas as pd
+
+# 讀取 Excel 檔案，並將其轉換為 DataFrame
+df = pd.read_excel('friendList.xlsx')
+
+# 更改 DataFrame 中的值
+df.at[len(df),0] = input('input data: ')
+
+# 將更改後的 DataFrame 寫回到 Excel 檔案
+df.to_excel('friendList.xlsx', index=False)
+"""
 import pandas as pd
 
-df1 = pd.read_excel('test.xlsx', sheet_name='工作表1')
+# 讀取 Excel 檔案，並將其轉換為 DataFrame
+df = pd.read_excel('friendList.xlsx')
 
+# 將資料表中的空白值填補為上一個非空白值
+df.fillna(method='ffill', inplace=True)
 
+# 將整理好的資料寫回到 Excel 檔案中
+df.to_excel('', index=False)
