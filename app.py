ff99d701cdf827da4a157b06d1fa32a41d16ea8e
XD <wesleylin30@gmail.com>
2023-04-28 18:01:17 +0800

    V11

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import schedule
+import time
 import pandas as pd
 import Tokens as t
 from flask import Flask, request, abort
@@ -38,10 +40,6 @@ def callback():
         abort(400)
 
     return 'OK'
-
-
-df1 = pd.read_excel("test.xlsx", sheet_name= '工作表1')
-df2 = pd.read_excel('compare.xlsx', sheet_name='工作表1')
 
 #使用者加入Bot後 傳送歡迎訊息 訊息要改成歡迎+自我介紹+功能說明
 @handler.add(FollowEvent)
@@ -88,23 +86,10 @@ def handle_unfollow(event):
         # 儲存更新後的Excel檔案
         df.to_excel('friendList.xlsx', index=False)
         print('已刪除user_id:', user_id)
+        
+df1 = pd.read_excel("test.xlsx", sheet_name= '工作表1')
+df2 = pd.read_excel('compare.xlsx', sheet_name='工作表1')
 
-
-
-diff = df1.compare(df2)#照寫
-if not diff.empty:
-    df1_over_limit = df1.query('碳排放 >= 30')
-    message = ""
-    for i, row_data in df1_over_limit.iterrows():
-        message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
-    df = pd.read_excel('friendList.xlsx',sheet_name= 'Sheet1')
-    for i ,row_data in df.iterrows():
-        line_bot_api.push_message(row_data['user_id'], TextSendMessage(text=message))
-    
-    # 將 DataFrame 儲存到 Excel 文件中
-    with pd.ExcelWriter('compare.xlsx') as writer:
-        df1.to_excel(writer, sheet_name='工作表1', index=False)
-    
 # 當接收到 MessageEvent (文字訊息) 時的處理函式
 @handler.add(MessageEvent, message=TextMessage)#根據取得數據需要去做修改
 def handle_message(event):
@@ -169,6 +154,24 @@ def handle_postback(event):
             if ci == row_data['城市']:
                 message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
         line_bot_api.push_message(event.source.user_id, TextSendMessage(text=message))
+
+def check_data():
+    diff = df1.compare(df2)#照寫
+    if not diff.empty:
+        df1_over_limit = df1.query('碳排放 >= 30')
+        message = ""
+        for i, row_data in df1_over_limit.iterrows():
+            message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
+        df = pd.read_excel('friendList.xlsx',sheet_name= 'Sheet1')
+        for i ,row_data in df.iterrows():
+            line_bot_api.push_message(row_data['user_id'], TextSendMessage(text=message))
         
+        # 將 DataFrame 儲存到 Excel 文件中
+        with pd.ExcelWriter('compare.xlsx') as writer:
+            df1.to_excel(writer, sheet_name='工作表1', index=False)
+schedule.every(10).seconds.do(check_data)
+
 if __name__ == "__main__":
     app.run()
+    schedule.run.pending()
+    time.sleep(1)
