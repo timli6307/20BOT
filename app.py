6bbdf79054d29e38ff2247cf3926a7c7af121921
XD <wesleylin30@gmail.com>
2023-04-29 01:12:01 +0800

    V69

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import requests
-import json
-import schedule
 import time
 import pandas as pd
 import Tokens as t
@@ -11,14 +8,12 @@ from linebot import (
 )
 from linebot.exceptions import (
     InvalidSignatureError,
-    LineBotApiError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,FollowEvent,UnfollowEvent,
-    TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction,PostbackEvent,MessageTemplateAction,
-    PostbackAction,
-    QuickReply, QuickReplyButton, MessageAction
+    TemplateSendMessage, ButtonsTemplate, PostbackTemplateAction,PostbackEvent
 )
+from multiprocessing import current_process
 
 app = Flask(__name__)
 
@@ -42,6 +37,7 @@ def callback():
     return 'OK'
 
 #使用者加入Bot後 傳送歡迎訊息 訊息要改成歡迎+自我介紹+功能說明
+
 @handler.add(FollowEvent)
 def handle_follow(event):
     user_id = event.source.user_id
@@ -155,7 +151,11 @@ def handle_postback(event):
                 message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
         line_bot_api.push_message(event.source.user_id, TextSendMessage(text=message))
 
+#print(f'[pid:{current_process().pid}] in {__name__}')
+
 def check_data():
+    df1 = pd.read_excel("test.xlsx", sheet_name= '工作表1')
+    df2 = pd.read_excel('compare.xlsx', sheet_name='工作表1')
     diff = df1.compare(df2)#照寫
     if not diff.empty:
         df1_over_limit = df1.query('碳排放 >= 30')
@@ -164,14 +164,21 @@ def check_data():
             message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
         df = pd.read_excel('friendList.xlsx',sheet_name= 'Sheet1')
         for i ,row_data in df.iterrows():
-            line_bot_api.push_message(row_data['user_id'], TextSendMessage(text=message))
+            line_bot_api.push_message(row_data['user_id'] ,TextSendMessage(text= message))
         
         # 將 DataFrame 儲存到 Excel 文件中
         with pd.ExcelWriter('compare.xlsx') as writer:
             df1.to_excel(writer, sheet_name='工作表1', index=False)
-schedule.every(10).seconds.do(check_data)
+
+
+def run1():
+    while True:
+        check_data()
+        time.sleep(1)
 
 if __name__ == "__main__":
+    
+    from multiprocessing import Process
+    check_data =Process(target=run1)
+    check_data.start()
     app.run()
-    schedule.run.pending()
-    time.sleep(1)
--- test.py
+++ test.py
@@ -1,21 +1,18 @@
-"""import pandas as pd
-
-# 讀取 Excel 檔案，並將其轉換為 DataFrame
-df = pd.read_excel('friendList.xlsx')
-
-# 更改 DataFrame 中的值
-df.at[len(df),0] = input('input data: ')
-
-# 將更改後的 DataFrame 寫回到 Excel 檔案
-df.to_excel('friendList.xlsx', index=False)
-"""
 import pandas as pd
-
-# 讀取 Excel 檔案，並將其轉換為 DataFrame
-df = pd.read_excel('friendList.xlsx')
-
-# 將資料表中的空白值填補為上一個非空白值
-df.fillna(method='ffill', inplace=True)
-
-# 將整理好的資料寫回到 Excel 檔案中
-df.to_excel('', index=False)
+df1 = pd.read_excel("test.xlsx", sheet_name= '工作表1')
+df2 = pd.read_excel('compare.xlsx', sheet_name='工作表1')
+def check_data():
+    global df1
+    diff = df1.compare(df2)#照寫
+    if not diff.empty:
+        df1_over_limit = df1.query('碳排放 >= 30')
+        message = ""
+        for i, row_data in df1_over_limit.iterrows():
+            message += f"工廠名稱: {row_data['工廠']}\n碳排放: {row_data['碳排放']}\n超標: {row_data['超標']}\n城市: {row_data['城市']}\n-------------\n"
+        df = pd.read_excel('friendList.xlsx',sheet_name= 'Sheet1')
+        for i ,row_data in df.iterrows():
+            print(message)
+        
+        # 將 DataFrame 儲存到 Excel 文件中
+        with pd.ExcelWriter('compare.xlsx') as writer:
+            df1.to_excel(writer, sheet_name='工作表1', index=False)
